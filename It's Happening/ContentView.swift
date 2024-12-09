33b053f10557c2662dcbb849f1b2f661c6539fc3
kelly <kellyribeiro096@icloud.com>
2024-12-09 11:35:23 +0100

    Initial Commit

---
+++ It's Happening/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  It's Happening
+//
+//  Created by Kelly Karen Ribeiro on 09/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ It's Happening/It_s_HappeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  It_s_HappeningApp.swift
+//  It's Happening
+//
+//  Created by Kelly Karen Ribeiro on 09/12/24.
+//
+
+import SwiftUI
+
+@main
+struct It_s_HappeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
